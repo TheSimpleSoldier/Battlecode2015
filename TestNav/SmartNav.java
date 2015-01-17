47734f11e1119dac33b81f49005dbb3fef82beac
Joshua Leger <legermjoshua@gmail.com>
2015-01-16 22:01:29 -0700

    Added virtual void tester

--- TestNav/SmartNav.java
+++ TestNav/SmartNav.java
@@ -18,6 +18,7 @@ public class SmartNav
     //this takes a spot on a void and analyzes the entire void
     public boolean analyzeVoid(int x, int y) throws GameActionException
     {
+        int round = Clock.getRoundNum();
         int[] dims = findBorders(x, y);
 
         if(dims == null)
@@ -25,24 +26,35 @@ public class SmartNav
             return false;
         }
 
-        int[][] area = createVoidMap(dims);
+        int[][] area = analyzeVirtualVoids(dims);
 
         if(area == null)
         {
             return false;
         }
 
+
+        int[][] newArea = createVoidMap(dims, area);
+
+        if(newArea == null)
+        {
+            return false;
+        }
+
+        System.out.println("rounds: " + (Clock.getRoundNum() - round));
+
         /*
         The following is purely for testing purposes
+        */
         if(rc.readBroadcast(12865) != 1)
         {
             rc.broadcast(12865, 1);
-            System.out.println("start");
-            for(int k = 0; k < dims.length; k++)
+            //System.out.println("start");
+            /*for(int k = 0; k < dims.length; k++)
             {
                 System.out.println(dims[k]);
-            }
-            for(int k = 0; k < area.length; k++)
+            }*/
+            /*for(int k = 0; k < area.length; k++)
             {
                 String line = "";
                 for(int a = 0; a < area[0].length; a++)
@@ -50,9 +62,27 @@ public class SmartNav
                     line += area[k][a] + " ";
                 }
                 System.out.println(line);
-            }
+            }*/
+
+            /*for(int k = 0; k < newArea.length; k++)
+            {
+                String line = "";
+                for(int a = 0; a < newArea[0].length; a++)
+                {
+                    if(newArea[k][a] == -1)
+                    {
+                        line += "9 ";
+                    }
+                    else
+                    {
+                        line += newArea[k][a] + " ";
+                    }
+                }
+                System.out.println(line);
+            }*/
             rc.broadcast(12865, 0);
-        }*/
+        }
+
 
         return true;
     }
@@ -102,9 +132,15 @@ public class SmartNav
 
         MapLocation startBugLocation = currentLoc.add(Direction.NONE);
         Direction firstDir = Direction.NORTH;
+        int count = 0;
         while(rc.senseTerrainTile(currentLoc.add(firstDir)) != TerrainTile.VOID)
         {
-            firstDir = firstDir.rotateRight();
+            firstDir = firstDir.rotateRight().rotateRight();
+            if(count > 5)
+            {
+                return dims;
+            }
+            count++;
         }
         firstDir = firstDir.opposite();
 
@@ -114,6 +150,7 @@ public class SmartNav
         do
         {
             lastDir = lastDir.rotateRight().rotateRight();
+            count = 0;
             while(rc.senseTerrainTile(currentLoc.add(lastDir)) != TerrainTile.VOID)
             {
                 if(rc.senseTerrainTile(currentLoc.add(lastDir)) == TerrainTile.UNKNOWN)
@@ -121,6 +158,11 @@ public class SmartNav
                     return null;
                 }
                 lastDir = lastDir.rotateLeft().rotateLeft();
+                if(count > 5)
+                {
+                    return dims;
+                }
+                count++;
             }
             currentLoc = currentLoc.add(lastDir);
             if(currentLoc.x < dims[0])
@@ -151,7 +193,7 @@ public class SmartNav
     and 2 for off map. If any of the spots are unknown, the function will return
     null since unknown spots mean we will not be able to do any more analysis.
      */
-    private int[][] createVoidMap(int[] dims)
+    private int[][] createVoidMap(int[] dims, int[][] area)
     {
         int height = dims[3] - dims[1] + 1;
         int width = dims[2] - dims[0] + 1;
@@ -211,7 +253,7 @@ public class SmartNav
         height = newDims[3] - newDims[1] + 1;
         width = newDims[2] - newDims[0] + 1;
 
-        int[][] area = new int[height][width];
+        int[][] newArea = new int[height][width];
 
         for(int k = 0; k < height; k++)
         {
@@ -219,24 +261,225 @@ public class SmartNav
             {
                 int currentX = a + newDims[0];
                 int currentY = k + newDims[1];
-                TerrainTile tile = rc.senseTerrainTile(new MapLocation(currentX, currentY));
 
-                if(tile == TerrainTile.NORMAL)
+                if(currentX >= dims[0] && currentX <= dims[2] &&
+                   currentY >= dims[1] && currentY <= dims[3])
                 {
-                    area[k][a] = 0;
-                }
-                else if(tile == TerrainTile.VOID)
-                {
-                    area[k][a] = 1;
-                }
-                else if(tile == TerrainTile.OFF_MAP)
-                {
-                    area[k][a] = 2;
+                    newArea[k][a] = area[k - (dims[1] - newDims[1])][a - (dims[0] - newDims[0])];
                 }
                 else
                 {
-                    //spot is unknown, so the function cannot finish
+                    TerrainTile tile = rc.senseTerrainTile(new MapLocation(currentX, currentY));
+
+                    if(tile == TerrainTile.NORMAL)
+                    {
+                        newArea[k][a] = 0;
+                    }
+                    else if(tile == TerrainTile.VOID)
+                    {
+                        newArea[k][a] = -1;
+                    }
+                    else if(tile == TerrainTile.OFF_MAP)
+                    {
+                        newArea[k][a] = -2;
+                    }
+                    else
+                    {
+                        //spot is unknown, so the function cannot finish
+                        return null;
+                    }
+                }
+            }
+        }
+
+        return newArea;
+    }
+
+    /*
+    This method takes in the void feature and sets up virtual voids wherever
+    there are bowls or inside corners so the bot will know to avoid them
+    This will be accomplished by simply iterating through each diagonal,
+    vertical, and horizontal line and seeing if any spots lie between two
+    voids and marking them.
+    This algorithm will also try to differentiate different virtual void areas,
+    for example setting one area to 3 and another to 4 that are disconnected.
+    The reason being that if our target is in a virtual void and we are in one,
+    but not the right one, we know to go around and try from a different way.
+    This will be accomplished by taking every void and setting all the ones next
+    to it equal to that one and propagating it around until each group has its
+    own number.
+    There is one known bug which is that if there is another void that falls in
+    the main void's rectangle, it may think there should be a virtual void. Instead
+    of adding checks to everything when it rarely happens, it will be handled by
+    the bug pather in that it will not enter it, but it will look through it still.
+     */
+    private int[][] analyzeVirtualVoids(int[] dims)
+    {
+        int height = dims[3] - dims[1] + 1;
+        int width = dims[2] - dims[0] + 1;
+
+        int[][] voidArea = new int[height][width];
+
+        //this is somewhat redundant as it was done higher up for a larger size, but seemed
+        //more efficient than rediscovering the void barriers and creating a new array from that
+        for(int k = 0; k < height; k++)
+        {
+            for(int a = 0; a < width; a++)
+            {
+                int currentX = a + dims[0];
+                int currentY = k + dims[1];
+                TerrainTile tile = rc.senseTerrainTile(new MapLocation(currentX, currentY));
+
+                if(tile == TerrainTile.VOID)
+                {
+                    voidArea[k][a] = -1;
+                }
+                else if(tile == TerrainTile.UNKNOWN)
+                {
                     return null;
+                }
+            }
+        }
+
+        //horizontal
+        for(int k = 0; k < height; k++)
+        {
+            int start = 0;
+            int end = width - 1;
+
+            while(voidArea[k][start] != -1 && start < end)
+            {
+                start++;
+            }
+            while(voidArea[k][end] != -1 && start < end)
+            { end--;
+            }
+
+            if(start != end)
+            {
+                for(int a = start + 1; a < end; a++)
+                {
+                    if(voidArea[k][a] != -1)
+                    {
+                        voidArea[k][a] = 1;
+                    }
+                }
+            }
+        }
+
+        //vertical
+        for(int k = 0; k < width; k++)
+        {
+            int start = 0;
+            int end = height - 1;
+
+            while(voidArea[start][k] != -1 && start < end)
+            {
+                start++;
+            }
+            while(voidArea[end][k] != -1 && start < end)
+            {
+                end--;
+            }
+
+            if(start != end)
+            {
+                for(int a = start + 1; a < end; a++)
+                {
+                    if(voidArea[a][k] != -1)
+                    {
+                        voidArea[a][k] = 1;
+                    }
+                }
+            }
+        }
+
+        //diagonal \
+        for(int k = height * -1 + 1; k < height; k++)
+        {
+            int start = 0;
+            int end = width - 1;
+
+            while((k + start < 0 || k + start >= height || voidArea[k + start][start] != -1) && start < end)
+            {
+                start++;
+            }
+            while((k + end < 0 || k + end >= height || voidArea[k + end][end] != -1) && start < end)
+            {
+                end--;
+            }
+
+            if(start != end)
+            {
+                for(int a = start + 1; a < end; a++)
+                {
+                    if(voidArea[k + a][a] != -1)
+                    {
+                        voidArea[k + a][a] = 1;
+                    }
+                }
+            }
+        }
+
+        //diagonal /
+        for(int k = height * -1 + 1; k < height; k++)
+        {
+            int start = 0;
+            int end = width - 1;
+
+            while((height - 1 - (k + start) < 0 || height - 1 - (k + start) >= height ||
+                   voidArea[height - 1 - (k + start)][start] != -1) && start < end)
+            {
+                start++;
+            }
+            while((height - 1 - (k + end) < 0 || height - 1 - (k + end) >= height ||
+                   voidArea[height - 1 - (k + end)][end] != -1) && start < end)
+            {
+                end--;
+            }
+
+            if(start != end)
+            {
+                for(int a = start + 1; a < end; a++)
+                {
+                    if(voidArea[height - 1 - (k + a)][a] != -1)
+                    {
+                        voidArea[height - 1 - (k + a)][a] = 1;
+                    }
+                }
+            }
+        }
+
+        int number = 3;
+        for(int k = 0; k < height; k++)
+        {
+            for(int a = 0; a < width; a++)
+            {
+                if(voidArea[k][a] == 1)
+                {
+                    voidArea = recursiveChangeNumber(voidArea, number, k, a);
+                    number++;
+                }
+            }
+        }
+
+        return voidArea;
+    }
+
+    private int[][] recursiveChangeNumber(int[][] area, int number, int x, int y)
+    {
+        area[x][y] = number;
+        for(int k = -1; k <= 1; k++)
+        {
+            for(int a = -1; a <= 1; a++)
+            {
+                if(x + k >= 0 && x + k < area.length &&
+                   y + a >= 0 && y + a < area[0].length)
+                {
+                    if(area[x + k][y + a] == 1)
+                    {
+                        area = recursiveChangeNumber(area, number, x + k, y + a);
+                    }
                 }
             }
         }
